c37f5ce2abe1bcdf63927409b650196827b2a21a
dominikwh94 <dominik.halley@googlemail.com>
2016-06-12 10:02:06 +0200

    Added functions
    Added some functions to display other informations, also cleaned up the
    code and located some functions to header file

--- header.h
+++ header.h
@@ -4,6 +4,7 @@
  */
 
 #include <iostream>
+#include <ostream>
 #include <wiringPi.h>
 #include <pcf8574.h>
 #include <lcd.h>
@@ -15,6 +16,7 @@
 #include <unistd.h>
 #include <fstream>
 #include <cctype>
+#include <vector>
 
 #define LCDOFS(x)(lcdofs + x)
 
@@ -45,4 +47,50 @@ string getTemperature(char const* path) {
 	cout << "Current Temperature: " << file_contents;
 
 	return file_contents;
+}
+
+// function to display usage help
+static void show_usage(string name, string argument) {
+	cerr << "Usage: " << argument << endl
+		 << "-datetime" << " = displays the current date and time" << endl
+		 << "-timetemp" << " = displays the current time and temperature" << endl
+		 << "-datetemp" << " = displays the current date and temperature" << endl;
+}
+
+void display(string line1, string line2) {
+	// get current temperature, replace 28-0415932c86ff with your id
+	temp = getTemperature("/sys/bus/w1/devices/28-0415932c86ff/w1_slave");
+	// build temperature string
+	temperature="Temp: ";
+	temperature.append(temp);
+	temperature.append(" Deg");
+	// convert string to char
+	ch_temp = temperature.c_str();
+	time_t timer;
+	char buffer_date[26];
+	char buffer_time[26];
+	struct tm* tm_info;
+
+	time(&timer);
+	tm_info=localtime(&timer);
+	// build string for time and date
+	strftime(buffer_date, 26, "Date: %d.%m.%Y", tm_info);
+	strftime(buffer_time, 26, "Time: %H:%M:%S", tm_info);
+	// output the information
+	if ((line1 == "date")&&(line2 == "time")) {
+		lcdPosition(fd, 0,0);
+		lcdPrintf(fd, buffer_date);
+		lcdPosition(fd, 0,1);
+		lcdPuts(fd, buffer_time);
+	} else if ((line1 == "time")&&(line2 == "temp")) {
+		lcdPosition(fd, 0,0);
+		lcdPrintf(fd, buffer_time);
+		lcdPosition(fd, 0,1);
+		lcdPuts(fd, ch_temp);
+	} else if ((line1 == "date")&&(line2 == "temp")) {
+		lcdPosition(fd, 0,0);
+		lcdPrintf(fd, buffer_date);
+		lcdPosition(fd, 0,1);
+		lcdPuts(fd, ch_temp);
+	}
 }
--- test.cpp
+++ test.cpp
@@ -12,53 +12,47 @@ void firstSetup() {
 	wiringPiSetup();
 }
 
-int main(int argc, char** argv) {
-	cout << "Raspi-Test" << endl;
-	// Run initialization void
-	firstSetup();
+int main(int argc, char* argv[]) {
 
-	// LCD-AREA START
-    pinMode(LCDOFS(1), OUTPUT);
-    pinMode(LCDOFS(3), OUTPUT);
-    digitalWrite(LCDOFS(1), 0);
-    digitalWrite(LCDOFS(3), 1);
+	if (argc < 2 ) {
+		show_usage("test", argv[0]);
+	} else {
+		cout << "Raspi-Test" << endl;
+		// Run initialization void
+		firstSetup();
 
-    fd = lcdInit(2, 16, 4, LCDOFS(0), LCDOFS(2), LCDOFS(4), LCDOFS(5), LCDOFS(6), LCDOFS(7), 0, 0, 0, 0);
-    lcdHome(fd);
-    lcdClear(fd);
-    // LCD-AREA END
+		// LCD-AREA START
+		pinMode(LCDOFS(1), OUTPUT);
+		pinMode(LCDOFS(3), OUTPUT);
+		digitalWrite(LCDOFS(1), 0);
+		digitalWrite(LCDOFS(3), 1);
 
-    // Output the text on LCD and debug message on shell
-    lcdPrintf(fd, "   Raspi-Watch      started!    ");
-    cout << "Raspi-Watch Started!" << endl;
+		fd = lcdInit(2, 16, 4, LCDOFS(0), LCDOFS(2), LCDOFS(4), LCDOFS(5), LCDOFS(6), LCDOFS(7), 0, 0, 0, 0);
+		lcdHome(fd);
+		lcdClear(fd);
+		// LCD-AREA END
 
-    lcdClear(fd);
+		// Output the text on LCD and debug message on shell
+		lcdPrintf(fd, "   Raspi-Watch      started!    ");
+		cout << "Raspi-Watch Started!" << endl;
 
+		lcdClear(fd);
+	}
 
-    while(1) {
-    	// get current temperature, replace 28-0415932c86ff with your id
-    	temp = getTemperature("/sys/bus/w1/devices/28-0415932c86ff/w1_slave");
-    	// build temperature string
-		temperature="Temp: ";
-		temperature.append(temp);
-		temperature.append(" Deg");
-		// convert string to char
-		ch_temp = temperature.c_str();
-    	time_t timer;
-    	char buffer_date[26];
-    	char buffer_time[26];
-    	struct tm* tm_info;
-
-    	time(&timer);
-    	tm_info=localtime(&timer);
-    	// build string for time and date
-    	strftime(buffer_date, 26, "Date: %d.%m.%Y", tm_info);
-    	strftime(buffer_time, 26, "Time: %H:%M:%S", tm_info);
-    	// output the information
-    	lcdPosition(fd, 0,0);
-    	lcdPrintf(fd, ch_temp);
-    	lcdPosition(fd, 0,1);
-    	lcdPuts(fd, buffer_time);
-
+    vector<string> args(argv, argv+argc);
+    for (size_t i=1; i< args.size(); ++i) {
+    	if (args[i] == "-datetime"){
+    		while(1) {
+				display("date", "time");
+			}
+    	} else if (args[i] == "-timetemp") {
+    		while(1) {
+    			display("time", "temp");
+    		}
+    	} else if (args[i] == "-datetemp") {
+    		while(1) {
+    			display("date", "temp");
+    		}
+    	}
     }
 }
